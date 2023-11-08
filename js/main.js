6de80249de6a942f911400513367c18f6cd9ff9f
FEHR <fabiohfehr@gmail.com>
2023-11-08 21:35:40 +0000

    commit inicial

---
+++ js/main.js
@@ -0,0 +1,27 @@
+$(document).ready(function (){
+
+    $('#form').validate({
+        rules: {
+            nome: {
+                required: true,
+            },
+            email: {
+                required: true,
+            },
+            telefone: {
+                required: true,
+            },
+            cpf: {
+                required: true,
+            },
+            cep: {
+                required: true,
+            },
+        },
+    })
+
+    $('#cep').mask('00000-000')
+    $('#telefone').mask('(00) 00000-0000')
+    $('#cpf').mask('000.000.000-00')
+
+})
